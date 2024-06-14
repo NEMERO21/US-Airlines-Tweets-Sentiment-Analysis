f5e176f504b1f8a45499461948dab6b621a3a83f
Shrestha Pundir <97607950+NEMERO21@users.noreply.github.com>
2024-06-14 02:33:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
 st.markdown("This app is a Streamlit dashboard to analyze the sentiment of Tweets 🐦")
 st.sidebar.markdown("This app is a Streamlit dashboard to analyze the sentiment of Tweets 🐦")
 
-data_url = r"https://www.kaggle.com/datasets/crowdflower/twitter-airline-sentiment"
+data_url = r"https://www.kaggle.com/datasets/serkanp/airlinetweets"
 
 @st.cache_data(persist = True)
 def load_data():
